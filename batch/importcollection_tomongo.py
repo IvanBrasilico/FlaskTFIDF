84db156d103729b9404cfcfa068434099bcbef52
IvanBrasilico <brasilico.ivan@gmail.com>
2018-01-03 11:23:39 -0200

    Testes MongoDB - testes de otimização

--- batch/importcollection_tomongo.py
+++ batch/importcollection_tomongo.py
@@ -8,6 +8,11 @@ Test module processtec
 """
 import pprint
 import time
+import platform
+import re
+import subprocess
+import os
+from collections import Counter, defaultdict
 from pymongo import MongoClient
 from pymongo import DeleteMany
 import batch.processtec as pt
@@ -26,23 +31,15 @@ def my_timer(function, description, params=[]):
 
 
 def mongo_init():
-    global collection
+    global collection, collection_word
     client = MongoClient()
     db = client.tecrank
     collection = db['TEC']
+    collection_word = db['TECwords']
 
 
-my_timer(mongo_init, 'Mongo initialization')
-
-# collection.remove()
-exists = True
-# collection, exists = manager.add_collection("TEC")
-if not exists:
-    listaTEC = my_timer(pt.montaTEC(), 'Montando lista TEC')
-    listaNCM = my_timer(pt.montaNCM(listaTEC), 'Montando lista NCM')
-    listaTECResumo = my_timer(pt.montaTECResumo(listaNCM),
-                              'Montando lista TEC Resumo')
-    # Create Documents
+def create_collection():
+    word_dict = Counter()
     for linha in listaTECResumo:
         codigo = linha[:10]
         descricao = linha[11:]
@@ -50,7 +47,33 @@ if not exists:
         collection.insert_one({'_id': codigo,
                                'descricao': descricao,
                                'words': words})
-        # document = manager.add_document(codigo, descricao)
+        for word in words:
+            word_dict[(codigo, word)] += 1
+    for key, value in word_dict.items():
+        collection_word.insert_one(
+            {'codigo': key[0],
+             'word': key[1],
+             'frequency': value
+             })
+
+
+my_timer(mongo_init, 'Mongo initialization')
+
+exists = True
+# collection, exists = manager.add_collection("TEC")
+if not exists:
+    collection.remove()
+    collection_word.remove()
+    listaTEC = my_timer(pt.montaTEC, 'Montando lista TEC')
+    listaNCM = my_timer(pt.montaNCM, 'Montando lista NCM', [listaTEC])
+    listaTECResumo = my_timer(pt.montaTECResumo,
+                              'Montando lista TEC Resumo', [listaNCM])
+    # Create Documents
+    my_timer(create_collection, 'Criando coleção MongoDB')
+    collection.create_index([('words', 1)])
+    collection_word.create_index([('codigo', 1), ('word', 1)])
+
+    # document = manager.add_document(codigo, descricao)
     # manager.commit()
     # Retrieve documents, process then
     # manager.process_collection(pt.tokenize_to_words) TODO
@@ -60,14 +83,14 @@ if not exists:
 
 
 def vocab():
-    # Retrieve vocabulary
-    global vocab
+    # Retrieve all vocabulary
     documents = collection.find()
-    vocab = set()
+    vocabulary = set()
     for document in documents:
-        vocab.update(set(document['words']))
-    print('Vocab', list(vocab)[:10])
-    print('Total de palavras: ', len(vocab))
+        vocabulary.update(set(document['words']))
+    print('Vocab ', list(vocabulary)[:10])
+    print('Total de palavras: ', len(vocabulary))
+    return vocabulary
 
 
 def word_count(words):
@@ -90,15 +113,22 @@ def tf(words):
     for word in words:
         agregate = list(collection.aggregate([
             {'$unwind': '$words'},
-            { '$match': {'words': word}},
+            {'$match': {'words': word}},
             {'$group':
-            {'_id': '$_id',
-            'count': {'$sum': 1}}
-            }
+             {'_id': '$_id',
+              'count': {'$sum': 1}}
+             }
         ]))
         # pprint.pprint(agregate)
         agregates.append(agregate)
     return agregates
+
+
+def tf2(words):
+    adict = defaultdict(dict)
+    for document in collection_word.find({'word': {'$in': words}}):
+        adict[document['word']][document['codigo']] = document['frequency']
+    return adict
 
 
 def collection_lenght():
@@ -121,15 +151,47 @@ def avg_dl():
     print('Média', agregate[0]['count'] / collection.count())
 
 
-words = ['carbonato', 'vestuario']
-my_timer(vocab, 'Listando vocabulário')
+"""
 my_timer(word_count, 'Vezes que carbonato ou vestuario ocorrem:',
          [words])
 my_timer(word_find, 'Carbonato ou vestuario ocorrem em:',
          [words])
-my_timer(collection_lenght, 'Tamanho da coleção')
-my_timer(avg_dl, 'Tamanho médio dos documentos')
-my_timer(tf, 'Contagem de palavras por documento', [words])
-
+Sequência necessária para fazer o TF/IDF
+Repetidos testes duraram em torno de 1 a 2 segundos no total do
+(em um notebook Celeron)
+Duas tarefas foram mais custosas:
+De 0.2 a 0.8 segundos foram gastos na contagem de palavras do vocabulário
+e mais 0.25 segundos para a contagem de ocorrências de cada palavra,
+isto sem índices.
+Após, foi feito um índice para as palavras do array words:
+   # collection.create_index([('words', 1)])
+ (ver log de testes em mongo_testes.txt)
+Aparentemente, o índice não fez grande diferença neste caso.
+Como a previsão é de pouca atualização e muita consulta, isto mostrou a
+necessidade de um 'cache' para estes dados, uma tabela para guardar o total
+de palavras, e outra para guardar uma chave palavra, lista de documentos
+com qtde de ocorrências
+"""
+print('##################################################')
+print('# Testes com uso de tabela RESUMO!!!')
+command = "cat /proc/cpuinfo"
+all_info = subprocess.check_output(command, shell=True).strip()
+all_info = all_info.decode()
+for line in all_info.split("\n"):
+    if "model name" in line:
+        print(re.sub(".*model name.*:", "", line, 1))
+print(os.uname())
+print(time.strftime('%Y-%m-%d %H:%M'))
+words = ['carbonato', 'vestuario', 'paracetamol', 'outros']
+print('Words', words)
+vocabulary = my_timer(vocab, '1 - Listando vocabulário e calculando tamanho')
+my_timer(collection_lenght, '2 - Calculando tamanho da coleção')
+my_timer(avg_dl, '3 - Calcular tamanho médio dos documentos')
+# my_timer(tf, '4 - Contagem de palavras por documento', [words])
+lista = my_timer(tf2, '4 - Contagem de palavras por documento', [words])
+print(lista['vestuario'])
 secse = time.time()
 print('Tempo total do script: ', secse - initial_sec)
+print('##################################################')
+
+# pprint.pprint(agregate[:9])
